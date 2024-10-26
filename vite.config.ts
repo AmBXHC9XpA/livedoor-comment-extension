201919e154710ca04cbbf31f66eaff5973305f88
AmBXHC9XpA <ambxhcxpa@gmail.com>
2024-10-26 19:12:54 +0900

    chore: remove unused code

--- vite.config.ts
+++ vite.config.ts
@@ -1,17 +1,6 @@
-import type { Plugin } from 'vite';
 import { defineConfig } from 'vite';
 import { crx, defineManifest } from '@crxjs/vite-plugin';
 import react from '@vitejs/plugin-react-swc';
-
-const viteManifestHackIssue846: Plugin & { renderCrxManifest: (manifest: any, bundle: any) => void } = {
-  // Workaround from https://github.com/crxjs/chrome-extension-tools/issues/846#issuecomment-1861880919.
-  name: 'manifestHackIssue846',
-  renderCrxManifest(_manifest, bundle) {
-    bundle['manifest.json'] = bundle['.vite/manifest.json'];
-    bundle['manifest.json'].fileName = 'manifest.json';
-    delete bundle['.vite/manifest.json'];
-  },
-};
 
 const manifest = defineManifest({
   manifest_version: 3,
@@ -42,7 +31,6 @@ const manifest = defineManifest({
 export default defineConfig({
   plugins: [
     react(),
-    viteManifestHackIssue846,
     crx({ manifest }),
   ],
 });
